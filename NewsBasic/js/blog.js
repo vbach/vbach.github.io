cb52da80b4e33ee7070d31a84e19ac90ee7b8e67
vbach <vlminder@student.fullsail.edu>
2018-06-26 18:55:05 -0400

    Adding website for assignment

---
+++ NewsBasic/js/blog.js
@@ -0,0 +1,45 @@
+document.querySelector("#blog h2").innerHTML = "Popular MMO Games";
+
+// use Ajax to load JSON file
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+    // Save the JSON data locally
+    var data = JSON.parse(xhr.responseText);
+
+    // Insert the HTML articles & into section ( insertAdjacentHTML )
+    var blogSection = document.querySelector('#blog');
+    if(blogSection){
+        var games = '';
+
+        for(var i = 0; i < data.games.length; i++) {
+            games += '<article>';
+
+
+            games += '<p class="thumbnail"><img src="' + data.games[i].image + '"alt ="' + data.games[i].name + '"></p>';
+            games += '<h3>' + data.games[i].name + '</h3>';
+
+            games += '<p>' + data.games[i].description + '</p>';
+
+            games += '<dl>';
+                games += '<dt>Rating</dt>';
+                games += '<dd>' + data.games[i].rating + ' out of 5 stars</dd>';
+                games += '<dt>Player Count</dt>';
+                games += '<dd>' + data.games[i].player_count + '</dd>';
+
+                games += '<dt>Servers</dt>';
+                games += '<dd>' + data.games[i].servers + '</dd>';
+             games += '</dl>';
+            games += '<p><a href="' + data.games[i].url + '">Read More</a></p>';
+
+
+            games += '</article>';
+
+        }
+
+        blogSection.querySelector('h2').insertAdjacentHTML('afterend', games);
+    }
+}
+
+// Call Ajax and close
+xhr.open('GET', 'https://vbach.github.io/data.json', true);
+xhr.send(null);
