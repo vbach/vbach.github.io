b61c9f7b570a67b0bccbebdeb8fea3af8fb522fe
vbach <vlminder@student.fullsail.edu>
2018-07-15 17:17:23 -0400

    .

--- Gallery/js/gallery.js
+++ Gallery/js/gallery.js
@@ -1,59 +1,70 @@
 // Change Title
 document.querySelector("#gallery h2").innerHTML = "World of Warcraft Gallery";
 
-var arrayOfImages = ["http://i67.tinypic.com/11ty5ux.jpg", "http://i65.tinypic.com/2ewdqva.jpg",
-    "http://i66.tinypic.com/o08d1k.jpg", "http://i66.tinypic.com/657qs3.jpg", "http://i67.tinypic.com/2ibn30g.jpg"];
-var arrayOfTitles = ["World of Warcraft: Legion"];
-var gallery = document.querySelector("#gallery img");
-var title = document.querySelectorAll('h3');
-var nextButton = document.getElementById('next');
-var previousButton = document.getElementById('previous');
-var i = 0;
+
+// use Ajax to load JSON file
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+    // Save the JSON data locally
+    var data = JSON.parse(xhr.responseText);
+
+    var gallery = document.querySelector("#gallery img");
+    var title = document.querySelectorAll('h3');
+    var alt = document.getElementById('alt');
+    var nextButton = document.getElementById('next');
+    var previousButton = document.getElementById('previous');
+    var i = 0;
+
 
 // Next Image
-function nextImage() {
-    i = i + 1;
-    i = i % arrayOfImages.length;
-    return arrayOfImages[i];
-}
+    function nextImage() {
+        i = i + 1;
+        i = i % data.games.length;
+        return data.games[i].image;
+    }
+
+// Next Title
+    function nextTitle() {
+        i = i + 1;
+        i = i % data.games.length;
+        return data.games[i].image_title;
+    }
 
 // Previous Image
-function previousImage() {
-    // Coding for previous image here
-    if (i === 0) {
-        i = arrayOfImages.length;
+    function previousImage() {
+        // Coding for previous image here
+        if (i === 0) {
+            i = data.games.length;
+        }
+        i = i - 1;
+        return data.games[i].image;
     }
-    i = i - 1;
-    return arrayOfImages[i];
-}
-
-// Add Title and Comment
-function titleAndComment(){
-    if (i === 0){
-        i = i + 1;
-        i = i % arrayOfTitles.length;
-        return arrayOfTitles[i];
-    }
-}
 
 // Setting the initial image of the array.
-gallery.src = arrayOfImages[i];
-title.innerHTML = arrayOfTitles[i];
+    gallery.src = data.games[i].image;
+    title.textContent = data.games[i].image_title;
+
 
 // Event listener for next button
-nextButton.addEventListener('click',
-    // On click run this function to retrieve data from nextImage function
-    function (e) {
-        gallery.src = nextImage();
-    }
-);
+    nextButton.addEventListener('click',
+        // On click run this function to retrieve data from nextImage function
+        function (e) {
+            gallery.src = nextImage();
+            title.textContent = nextTitle();
+        }
+    );
 
 // Event listener for previous button
-previousButton.addEventListener('click',
-    // On click run this function to retrieve data from previousImage function
-    function (e) {
-        gallery.src = previousImage();
-    }
-);
+    previousButton.addEventListener('click',
+        // On click run this function to retrieve data from previousImage function
+        function (e) {
+            gallery.src = previousImage();
+        }
+    );
+}
+
+// Call Ajax and close
+xhr.open('GET', 'https://vbach.github.io/Gallery/js/gallery.json', true);
+xhr.send(null);
 
 
