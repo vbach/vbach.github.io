d0a868b27ae89e1a9aee2f3dbb174eda3ee5dc95
vbach <vlminder@student.fullsail.edu>
2018-09-19 20:39:35 -0400

    Fixed html semantics, css, and progress on JS interactions.

--- dws1/js/events.js
+++ dws1/js/events.js
@@ -3,17 +3,64 @@
  * 
 */
 
-/* Modal Forms */
-var login = document.getElementById('loginBtn');
+/* Login and Sign Up Pop Up Forms */
+// Open Login
+var popUp = function(e) {
+    e.preventDefault();
+    document.getElementById("login").style.display = "block";
+};
 
-login.onclick = function () {
-    document.getElementById('signUpForm').style.display='none';
-    document.getElementById('login').style.display='block';
-}
+document.addEventListener('DOMContentLoaded', function(){
+    var login = document.getElementById('loginLink');
+    login.addEventListener('click', popUp);
+});
 
-var signUp = document.getElementById('signUpBtn');
 
-signUp.onclick = function(){
-    document.getElementById('login').style.display='none';
-    document.getElementById('signUpForm').style.display='block';
-}
+/* Form Validation */
+var validateForm = function(submitEvent){
+
+    if(!submitEvent.target.checkValidity()) {
+        submitEvent.preventDefault();
+        submitEvent.stopImmediatePropagation();
+        submitEvent.stopPropagation();
+
+        var form = submitEvent.target,
+            elements = form.elements;
+
+        for(var i = 0, len = elements.length; i < len; i++){
+            var element = elements[i];
+
+            if(element.willValidate === true && element.validity.valid !== true){
+                var message = 'Please do not leave blank',
+                    parent = element.parentNode,
+                    div = document.createElement('div');
+
+                div.appendChild(document.createTextNode(message));
+                div.classList.add('validation-message');
+
+                parent.insertBefore(div, element.previousSibling);
+
+                element.focus();
+                break;
+            }
+        }
+
+    } else {
+        return true;
+    }
+
+
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var forms = document.querySelectorAll('form');
+
+    for (var i = forms.length -1; i >= 0; i--) {
+        var form = forms[i];
+
+        form.noValidate = true;
+        form.addEventListener('submit', validateForm);
+    }
+});
+
+
