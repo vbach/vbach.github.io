f6dadece0f910be9107a8b08ce7955d564dbfd49
vbach <vlminder@student.fullsail.edu>
2019-02-07 13:13:35 -0500

    Revision 2

--- munlu/js/video.js
+++ munlu/js/video.js
@@ -1,7 +1,6 @@
 // Mute/Turn Off Mute Video Index.html
 const muteOffBtn = document.querySelector('.parallaxvideo'),
-
-    video = document.getvideoById('index-video');
+    video = document.getElementById('index-video');
 chngBtn = document.querySelector('.muteBtnStyle span');
 
 muteOffBtn.addEventListener('click', () => {
@@ -15,16 +14,3 @@ muteOffBtn.addEventListener('click', () => {
         chngBtn.setAttribute('class', 'fas fa-volume-up')
     }
 });
-
-const maxBtnStyle = document.querySelector('.maxBtnStyle');
-maxBtnStyle.addEventListener('click', (e) => {
-    if (video.requestFullscreen) {
-        video.requestFullscreen
-    } else if (video.webkitRequestFullscreen) {
-        video.webkitRequestFullscreen();
-    } else if (video.mozRequestFullScreen) {
-        video.mozRequestFullScreen();
-    } else if (video.msRequestFullscreen) {
-        video.msRequestFullscreen();
-    }
-})
